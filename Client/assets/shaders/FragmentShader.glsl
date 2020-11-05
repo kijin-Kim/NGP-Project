147a296be21808dc213de189385673ec76f8fa7d
kijin-kim <kijin.kim0@gmail.com>
2020-11-06 01:09:25 +0900

    Create Game, Renderer, Shader class and default shader

---
+++ Client/assets/shaders/FragmentShader.glsl
@@ -0,0 +1,9 @@
+#version 330 core
+
+uniform vec4 u_Color;
+out vec4 outColor;
+
+void main()
+{
+	outColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+}
--- Client/main.cpp
+++ Client/main.cpp
@@ -1,52 +1,8 @@
-#include <GL/glew.h>
-#include <stdio.h>
-#include <GLFW/glfw3.h>
-#include <WinSock2.h>
-
+#include "Game.h"
 
 int main(void)
 {
-
-	GLFWwindow* window;
-
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-
-	GLenum err = glewInit();
-	if (GLEW_OK != err)
-	{
-		/* Problem: glewInit failed, something is seriously wrong. */
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-	}
-	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
-
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
-
-	return 0;
+	Game* game = new Game(640, 480);
+	game->Run();
+	delete game;
 }
