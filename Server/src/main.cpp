0dd15eb31bf413f53c238887c8dced7ca0a2d1b7
Son Dong Kyu <fkffksk20@gmail.com>
2020-11-25 00:46:09 +0900

    mainServer

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,6 +1,13 @@
 #include "Network/Network.h"
 
+
+#define MAX_USER 4
 DWORD ListeningThread(LPVOID);
+DWORD ProcessRecvThread(LPVOID arg);
+
+int ClientCount = 0;
+HANDLE m_hClientsThreads[MAX_USER];// clients threads
+
 
 int main()
 {
@@ -9,14 +16,18 @@ int main()
 	network->isServer = true;
 	network->BindAndListen();
 	HANDLE hThread;
+
 	while (1) {
-		hThread = CreateThread(NULL, 0, ListeningThread, (LPVOID)network->m_ClientSock, 0, NULL);
+		hThread = CreateThread(NULL, 0, ListeningThread,
+			(LPVOID)network->m_ClientSock, 0, NULL);
+
 		if (hThread == NULL) {
 			closesocket(network->m_ClientSock);
 		}
 		else {
 			CloseHandle(hThread);
 		}
+
 	}
 	return 1;
 }
@@ -25,39 +36,43 @@ DWORD ListeningThread(LPVOID)
 {
 	Network* network = Network::GetInstance();
 	char buf[BUFSIZE + 1];
+	int myClientID = 0;
+	int id = 0;
+
 	while (1)
 	{
 		network->Accept();
-		printf("[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
-			inet_ntoa(network->m_ClientAddr.sin_addr), ntohs(network->m_ClientAddr.sin_port));
+		printf("LT_[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+			inet_ntoa(network->m_ClientAddr.sin_addr),
+			ntohs(network->m_ClientAddr.sin_port));
 		network->ClientInfo();
 
-		while (1)
+
+		m_hClientsThreads[id] = CreateThread(NULL, 0, ProcessRecvThread,
+			(LPVOID)network->m_ClientSock,0,NULL);
+		if (NULL == m_hClientsThreads[id]) 
 		{
-			network->Recv(buf, BUFSIZE);
-			if (network->retval == SOCKET_ERROR) {
-				network->ErrDisplay(L"recv()");
-				break;
-			}
-			else if (network->retval == 0)
-				break;
-
-			buf[network->retval] = '\0';
-			printf("[TCP/%s:%d] %s\n", inet_ntoa(network->m_ClientAddr.sin_addr),
-				ntohs(network->m_ClientAddr.sin_port), buf);
-
-			printf("LT_[TCP 클라이언트] %d바이트를 받았습니다.\n", network->retval);
-
-			network->Send(buf, BUFSIZE);
-			if (network->retval == SOCKET_ERROR) {
-				network->ErrDisplay(L"send()");
-				break;
-			}
+			closesocket(network->m_ClientSock);
 		}
-
-		closesocket(network->m_ClientSock);
-
 	}
 
 	network->Release(network->m_Sock);
+}
+
+DWORD ProcessRecvThread(LPVOID arg)
+{
+	Network* network = Network::GetInstance();
+
+	int id = reinterpret_cast<int>(arg);
+	printf("TCP 접속, ID : %d\n", id);
+
+	SOCKADDR_IN clientAddr;
+	int addrlen;
+
+	// 클라이언트 정보 얻기
+	addrlen = sizeof(clientAddr);
+	getpeername(network->m_ClientSock,
+		(SOCKADDR*)&clientAddr, &addrlen);
+	
+	return 1;
 }
