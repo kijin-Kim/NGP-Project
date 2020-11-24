45932b7206049a417ace2155dc1c884410bd4c50
omegabunh <omegabun@gmail.com>
2020-11-24 16:40:46 +0900

    fix send recv and server

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,8 +1,4 @@
-#pragma comment(lib, "ws2_32")
-#include <winsock2.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include<WinSock2.h>
+#include "Network/Network.h"
 #include<iostream>
 #include<string>
 #include<mutex>
@@ -264,3 +260,49 @@ DWORD SendThread(LPVOID arg)
 	}
 	return 0;
 }
+
+//#include "Network/Network.h"
+//
+//int main()
+//{
+//	char buf[BUFSIZE + 1];
+//	Network* network = Network::GetInstance();
+//	network->isServer = true;
+//	network->BindAndListen();
+//
+//	while (1)
+//	{
+//		network->Accept();
+//		printf("[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+//			inet_ntoa(network->m_ClientAddr.sin_addr), ntohs(network->m_ClientAddr.sin_port));
+//		network->ClientInfo();
+//
+//		while (1)
+//		{
+//			network->Recv(buf, BUFSIZE);
+//			if (network->retval == SOCKET_ERROR) {
+//				network->ErrDisplay(L"recv()");
+//				break;
+//			}
+//			else if (network->retval == 0)
+//				break;
+//
+//			buf[network->retval] = '\0';
+//			printf("[TCP/%s:%d] %s\n", inet_ntoa(network->m_ClientAddr.sin_addr),
+//				ntohs(network->m_ClientAddr.sin_port), buf);
+//
+//			printf("[TCP 클라이언트] %d바이트를 받았습니다.\n", network->retval);
+//
+//			network->Send(buf, BUFSIZE);
+//			if (network->retval == SOCKET_ERROR) {
+//				network->ErrDisplay(L"send()");
+//				break;
+//			}
+//		}
+//
+//		closesocket(network->m_ClientSock);
+//
+//	}
+//
+//	network->Release(network->m_Sock);
+//}
